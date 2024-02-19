d268992b2fbb9478ac47243d70c547f6645f1184
DH Asik <delwarhossenasik@gmail.com>
2024-02-19 12:21:18 +0600

    Call back function used in set state | Added functionality in jsx | Real time state updateing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useState } from "react"
+const test=['Upload now..','Review it..','Submit now']
+export default function App(){
+    const [Step,setStep]=useState(1)
+    const [isOpen,setOpen]=useState(true)
+    function PrevHandle(){
+        if(Step>1){
+            setStep(x=>x-1)
+        }
+        // if(Step>1){
+        //     setStep(Step-1)
+        // }
+    }
+    function NextHandle(){
+        if(Step<3){
+            setStep(x=>x+1)
+        }
+        // if(Step<3){
+        //     setStep(Step+1)
+        // }
+    }
+    return (
+    <>
+    <button onClick={()=>{
+        setOpen(x=>!x)
+    }}>&times;</button>
+    {isOpen && <div className="Box">
+            <div className="InnerBox">
+                <div className={`number ${Step>= 1? 'Active':""}`}>1</div>
+                <div className={`number ${Step>= 2? 'Active':""}`}>2</div>
+                <div className={`number ${Step>= 3? 'Active':""}`}>3</div>
+            </div>
+            <p style={{textAlign:"center"}}>{test[Step-1]}</p>
+            <div className="InnerBox">
+                <div className='btn' onClick={PrevHandle}>Prev</div>
+                <div className='btn' onClick={NextHandle}>Next</div>
+            </div>
+        </div>}
+    </>)
 }
-
-export default App;
